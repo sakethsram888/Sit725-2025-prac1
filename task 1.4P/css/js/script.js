3c240041ca3c8916ebbb0e5b91771835eb5b0ce6
sakethsram888 <s225092561@deakin.edu.au>
2025-03-08 18:20:16 +1100

    commit 2

--- task 1.4P/css/js/script.js
+++ task 1.4P/css/js/script.js
@@ -1,5 +1,5 @@
 function changeText() {
-    var textsArray = ['Text 1', 'Text 2', 'Text 3', 'Text 4', 'Text 5'];
+    var textsArray = ['applied', 'software', 'engineering', 'is', 'unit'];
   
     var number = getRandomNumberBetween(0, textsArray.length - 1);
   
