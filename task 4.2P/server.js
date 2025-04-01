3e5d75de1b3bb8436df135a233f15d485da488c2
sakethsram888 <s225092561@deakin.edu.au>
2025-04-01 19:19:35 +1100

    commit 6

--- task 4.2P/server.js
+++ task 4.2P/server.js
@@ -1,9 +1,31 @@
+const mongoose = require('mongoose');
 var express = require("express")
 var app = express()
 
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb://localhost:27017/myprojectDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    });
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB!');
+});
+
+const ProjectSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    link: String,
+    description: String,
+    });
+    const Project = mongoose.model('Project', ProjectSchema);
+
+app.get('/api/projects', async (req, res) => {
+const projects = await Project.find({});
+res.json({ statusCode: 200, data: projects, message: "Success" });
+});
 
 const cardList = [
     {
@@ -21,12 +43,17 @@ const cardList = [
 
 ]
 
-app.get('/api/projects',(req,res) => {
-    res.json({statusCode: 200, data: cardList, message:"Success"})
-})
-
 var port = process.env.port || 3009;
 
 app.listen(port,()=>{
     console.log("App running at http://localhost:"+port)
 })
+
+const sampleProject = new Project({
+    title: "Kitten 4",
+    image: "images/kitten-4.jpg",
+    link: "About Kitten 4",
+    description: "Demo description about kitten 4"
+});
+sampleProject.save().then(() => console.log("Sample project saved!"));
+    
